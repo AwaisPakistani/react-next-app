e996d2d8dd02b3139b39f641f8fa2d7df5e89967
anjum <awaisprojects1@gmail.com>
2022-12-15 02:00:29 +0500

    First Commit

---
+++ pages/about.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+import Base from './base'
+
+export default function Home() {
+  return (
+    <Base>
+      
+
+      <h1>About</h1>
+      <Link href="/">Home</Link>
+
+    
+    </Base>
+  )
+}
